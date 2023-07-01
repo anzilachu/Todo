21edac1f063519a4a548302e56475ffa3abcb6bd
ANZIL K <anzilmac@ANZILs-MacBook-Pro.local>
2023-07-02 00:05:40 +0530

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.add,name='add'),
+    path('delete/<int:id>/',views.delete,name='delete'),
+    path('edit/<int:id>/',views.edit,name='edit'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.TaskDetailview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.TaskUpdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.TaskDeleteview.as_view(),name='cbvdelete'),
+    # path('details/',views.details,name='details'),
+]
---
+++ todoapp/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render,redirect
+from . models import Task
+from .forms import TodoForm
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+from django.urls import reverse_lazy
+
+
+
+class Tasklistview(ListView):
+    model=Task
+    template_name='home.html'
+    context_object_name='task1'
+
+class TaskDetailview(DetailView):
+    model=Task
+    template_name='detail.html'
+    context_object_name='task'
+
+class TaskUpdateview(UpdateView):
+    model=Task
+    template_name='edit.html'
+    context_object_name='task'
+    fields=('name','priority','date')
+
+    def get_success_url(self):
+        return reverse_lazy('cbvdetail',kwargs={'pk':self.object.id})
+
+class TaskDeleteview(DeleteView):
+    model=Task
+    template_name='delete.html'
+    success_url=reverse_lazy('cbvhome')
+
+
+def add(request):
+    task1=Task.objects.all()
+    if request.method=='POST':
+        name=request.POST.get('task')
+        priority=request.POST.get('priority')
+        date=request.POST.get('date')
+        task=Task(name=name,priority=priority,date=date)
+        task.save()
+    return render(request,'home.html',{'task1':task1})
+
+
+# def details(request):
+    
+#     return render(request,'detail.html',{'task':task})
+
+def delete(request,id):
+    task=Task.objects.get(id=id)
+    if request.method=='POST':
+        task.delete()
+        return redirect('/')
+    return render(request,'delete.html',{'task':task})
+    
+def edit(request,id):
+    task=Task.objects.get(id=id)
+    form=TodoForm(request.POST or None,instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'update.html',{'task':task,'form':form})
